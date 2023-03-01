7c136c505f6ff1ec071e062713deee75b0593ff1
rotemlevibigabid <87079441+rotemlevibigabid@users.noreply.github.com>
2023-03-01 12:42:35 +0200

    support ssc schema protocol

--- src/neo4j-config.interface.ts
+++ src/neo4j-config.interface.ts
@@ -1,4 +1,4 @@
-export type Neo4jScheme = 'neo4j' | 'neo4j+s' | 'neo4j+scc' | 'bolt' | 'bolt+s' | 'bolt+scc'
+export type Neo4jScheme = 'neo4j' | 'neo4j+s' | 'neo4j+scc' | 'neo4j+ssc' | 'bolt' | 'bolt+s' | 'bolt+scc' | 'bolt+ssc'
 
 export interface Neo4jConfig {
     scheme: Neo4jScheme;
